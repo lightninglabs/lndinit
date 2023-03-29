9278816c4cc5c82854887efca6b978133af1a4d5
Andras Banki-Horvath <bhandras@gmail.com>
2023-03-29 15:09:33 +0200

    build: bump lnd to v0.16.0-beta

--- version.go
+++ version.go
@@ -32,7 +32,7 @@ const (
 	AppMinor uint = 1
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 9
+	AppPatch uint = 10
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
