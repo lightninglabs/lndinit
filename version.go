88e14b59609d4bbff6b3e9ace128e346b8a0f4f2
Calvin Zachman <calvin.zachman@protonmail.com>
2023-09-20 09:36:16 -0500

    build: bump to lnd v0.17.0-beta.rc4

--- version.go
+++ version.go
@@ -32,7 +32,7 @@ const (
 	AppMinor uint = 1
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 16
+	AppPatch uint = 17
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
