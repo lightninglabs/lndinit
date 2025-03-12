27dd316e048f25392d199a949b11430e8bd9d9dc
ziggie <ziggie1984@protonmail.com>
2025-03-12 16:52:59 +0100

    logs: update logger to use btclog v2

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/lightninglabs/lndinit
 
 require (
 	github.com/btcsuite/btcd v0.24.3-0.20250318170759-4f4ea81776d6
+	github.com/btcsuite/btclog v0.0.0-20241003133417-09c4e92e319c
+	github.com/btcsuite/btclog/v2 v2.0.1-0.20250110154127-3ae4bf1cb318
 	github.com/btcsuite/btcwallet v0.16.12
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/kkdai/bstream v1.0.0
@@ -26,12 +28,10 @@ require (
 	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
 	github.com/btcsuite/btcd/btcutil/psbt v1.1.8 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
-	github.com/btcsuite/btclog v0.0.0-20241003133417-09c4e92e319c // indirect
-	github.com/btcsuite/btclog/v2 v2.0.1-0.20250110154127-3ae4bf1cb318 // indirect
 	github.com/btcsuite/btcwallet/wallet/txauthor v1.3.5 // indirect
 	github.com/btcsuite/btcwallet/wallet/txrules v1.2.2 // indirect
 	github.com/btcsuite/btcwallet/wallet/txsizes v1.2.5 // indirect
-	github.com/btcsuite/btcwallet/walletdb v1.4.4 // indirect
+	github.com/btcsuite/btcwallet/walletdb v1.4.5-0.20250311184728-a7bb395324e8 // indirect
 	github.com/btcsuite/btcwallet/wtxmgr v1.5.4 // indirect
 	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
 	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
--- log.go
+++ log.go
@@ -1,19 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"time"
+
+	btclogv1 "github.com/btcsuite/btclog"
+	"github.com/btcsuite/btclog/v2"
 )
 
-type logger func(format string, args ...interface{})
+var (
+	// backend is the logging backend used to create all loggers.
+	backend = btclog.NewDefaultHandler(os.Stderr)
 
-func stderrLogger(format string, args ...interface{}) {
-	formattedMsg := fmt.Sprintf(format, args...)
-	now := time.Now().Format("2006-01-02 15:04:05.000")
-	_, _ = fmt.Fprintf(
-		os.Stderr, "%s LNDINIT: %s\n", now, formattedMsg,
-	)
+	// logger is logger for the main package of the lndinit tool.
+	logger = btclog.NewSLogger(backend).WithPrefix("LNDINIT")
+)
+
+// Initialize sets up all loggers with their default levels.
+func Initialize(debugLevel btclogv1.Level) {
+	// Set default log level for all loggers
+	level := btclogv1.LevelInfo
+	if debugLevel.String() != "" {
+		var err error
+		backend.SetLevel(debugLevel)
+		if err != nil {
+			logger.Errorf("Invalid debug level %s, "+
+				"using info", debugLevel)
+			level = btclog.LevelInfo
+		}
+	}
+
+	logger.SetLevel(level)
 }
 
-func noopLogger(_ string, _ ...interface{}) {}
+// NewSubLogger creates a new sub logger with the given prefix.
+func NewSubLogger(prefix string) btclog.Logger {
+	return logger.SubSystem(prefix)
+}
