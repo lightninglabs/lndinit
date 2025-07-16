9aee1ad23406a2565061673503a00182b2d52ce2
Calvin Zachman <calvin.zachman@protonmail.com>
2025-07-16 10:47:53 -0400

    build: bump lnd to v0.19.2-beta

--- version.go
+++ version.go
@@ -32,7 +32,7 @@ const (
 	AppMinor uint = 1
 
 	// AppPatch defines the application patch for this binary.
-	AppPatch uint = 29
+	AppPatch uint = 30
 
 	// AppPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
